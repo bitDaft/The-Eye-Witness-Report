3ff9ba292ca5d670c7ee21e7942f74366d1d2936
Tausif ali <ajaxhis@tutanota.com>
2020-06-20 00:23:03 +0530

    added search bar and topics component

---
+++ src/components/basic/SearchTopics/template.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./template.scss";
+
+// function TemplateFuncComponent(props) {
+//   return (
+//     <div></div>
+//   );
+// }
+
+class SearchTopics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: false,
+      searchQuery: props.value,
+    };
+    this.ref = null;
+  }
+
+  toggleSearchBar = (e) => {
+    if (!this.state.search) {
+      this.ref.focus();
+    }
+    this.setState(
+      (state) => ({ search: !this.state.search, searchQuery: "" }),
+      () => {
+        this.ref.value = "";
+        this.props.onChange({
+          target: {
+            name: this.props.name,
+            value: "",
+          },
+        });
+      }
+    );
+  };
+  squishSearch = () => {
+    window.setTimeout(() => {
+      if (!this.props.value) {
+        this.setState((state) => ({ search: false }));
+      }
+    }, 100);
+  };
+
+  render() {
+    let searchClasses = "search ";
+    if (this.state.search) {
+      searchClasses += "active ";
+    }
+    let icon = !this.state.search ? "fas fa-search" : "fas fa-times";
+    return (
+      <div className="search-topics">
+        <ul>
+          <li>US</li>
+          <li>WORLD</li>
+          <li>ARTS</li>
+        </ul>
+        <div className={searchClasses}>
+          <input
+            ref={(ref) => (this.ref = ref)}
+            type="text"
+            onBlur={this.squishSearch}
+            name={this.props.name}
+            value={this.props.value}
+            onChange={this.props.onChange}
+            placeholder="Search articles ..."
+          />
+          <div className="search-icon" onClick={this.toggleSearchBar}>
+            <i className={icon} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchTopics;
