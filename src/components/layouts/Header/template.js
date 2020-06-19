4fd2afa9a621b9b67fa6f5aa939d01445b98e64f
Tausif ali <ajaxhis@tutanota.com>
2020-06-20 00:24:13 +0530

    added header component

---
+++ src/components/layouts/Header/template.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./template.scss";
+
+// function TemplateFuncComponent(props) {
+//   return (
+//     <div></div>
+//   );
+// }
+
+class TemplateClassComponent extends React.Component {
+  render() {
+    const today = new Date();
+    return (
+      <div className="header">
+        <div className="pop-up">=</div>
+        <div className="logo">
+          <div>The Eye Witness Report</div>
+          <div>
+            <span>
+              {today.toDateString()} | Showing {this.props.text}'s paper
+            </span>
+            <i className="fas fa-angle-down" />
+            <select value={this.props.value} onChange={this.props.onChange}>
+              {this.props.periods.map((period) => {
+                return <option key={period.period} value={period}>{period.text}</option>;
+              })}
+            </select>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TemplateClassComponent;
