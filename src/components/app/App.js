f7343ea1fec6eb599b6ea363543e8dba5530c62c
Tausif ali <ajaxhis@tutanota.com>
2020-06-20 00:25:17 +0530

    removed test code to its own component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 
-import { Switch, Route, Link } from "react-router-dom";
-
-
+import { Switch, Route } from "react-router-dom";
 
 import Test from 'components/basic/test';
+
+
 
 function App() {
   
--- src/components/basic/test/template.js
+++ src/components/basic/test/template.js
@@ -1,23 +1,18 @@
 import React from "react";
 import "./template.scss";
 
-import BasicNewsCard from "components/basic/BasicNewsCard";
 
-import logo from "assets/images/logo.svg";
+import Home from 'components/views/Home'
+
+
 
 class Test extends React.Component {
-  article = {
-    img: "https://static01.nyt.com/images/2020/06/17/us/politics/17dc-bolton/17dc-bolton-threeByTwoSmallAt2X.jpg?quality=75&auto=webp&disable=upscale&width=400",
-    title:
-      "Memoir Accuses President of Seeking China’s Help to Win Re-Election",
-    description:
-      "In his new book, John Bolton, the former national security adviser, describes episodes where President Trump sought to halt criminal inquiries.",
-  };
+
 
   render() {
     return (
       <div className="test">
-        <BasicNewsCard article={this.article} />
+        <Home />
       </div>
     );
   }
