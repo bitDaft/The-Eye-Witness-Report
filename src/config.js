6e73ec65530239b8bf573e35452dfe2245cc3dbf
Tausif ali <ajaxhis@tutanota.com>
2020-06-20 00:20:47 +0530

    added fontawesome, config file and global styles

---
+++ src/config.js
@@ -0,0 +1,24 @@
+export default {
+  env: "dev",
+  dev: {
+    api_key: "2QbGF5HasLA7kpVjV9j9vpv4rOYNv1Jd",
+    top_story_endpoint: "topstories/v2",
+    most_popular_endpoint: "mostpopular/v2",
+    search_endpoint: "search/v2/articlesearch.json",
+    base_url: "https://api.nytimes.com/svc",
+  },
+  staging: {
+    api_key: "2QbGF5HasLA7kpVjV9j9vpv4rOYNv1Jd",
+    top_story_endpoint: "topstories/v2",
+    most_popular_endpoint: "mostpopular/v2",
+    search_endpoint: "search/v2/articlesearch.json",
+    base_url: "https://api.nytimes.com/svc",
+  },
+  prod: {
+    api_key: "2QbGF5HasLA7kpVjV9j9vpv4rOYNv1Jd",
+    top_story_endpoint: "topstories/v2",
+    most_popular_endpoint: "mostpopular/v2",
+    search_endpoint: "search/v2/articlesearch.json",
+    base_url: "https://api.nytimes.com/svc",
+  },
+};
