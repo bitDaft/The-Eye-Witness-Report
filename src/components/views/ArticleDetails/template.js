661ca49cb3ba1ce75a0a248ad6c33f2357240692
Tausif ali <ajaxhis@tutanota.com>
2020-06-20 02:37:08 +0530

    added article details view page

---
+++ src/components/views/ArticleDetails/template.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './template.scss';
+
+// function TemplateFuncComponent(props) {
+//   return (
+//     <div></div>
+//   );
+// }
+
+class ArticleDetails extends React.Component {
+  render (){
+    return (
+      <div className="article-details">
+        article details
+      </div>
+    );
+  }
+}
+
+export default ArticleDetails;
