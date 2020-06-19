20f187efe0a2fff3829105e9fb04c41614c5ca73
Tausif ali <ajaxhis@tutanota.com>
2020-06-20 02:37:46 +0530

    restructured header component

--- src/components/layouts/Header/template.js
+++ src/components/layouts/Header/template.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./template.scss";
 
+import { Link } from "react-router-dom";
+
 // function TemplateFuncComponent(props) {
 //   return (
 //     <div></div>
@@ -10,32 +12,36 @@ import "./template.scss";
 class TemplateClassComponent extends React.Component {
   render() {
     const today = new Date();
+    let period_section =
+      this.props.onlyHeader === true ? null : (
+        <div>
+          <span>
+            {today.toDateString()} | Showing {this.props.text}'s paper
+          </span>
+          <i className="fas fa-angle-down" />
+          <select
+            value={this.props.value}
+            onChange={this.props.onChange}
+            name={this.props.name}
+          >
+            {this.props.periods.map((period) => {
+              return (
+                <option key={period.period} value={period.period}>
+                  {period.text}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      );
     return (
       <div className="header">
         <div className="pop-up">=</div>
         <div className="logo">
-          <div>The Eye Witness Report</div>
-          {this.props.onlyHeader === true ? null : (
-            <div>
-              <span>
-                {today.toDateString()} | Showing {this.props.text}'s paper
-              </span>
-              <i className="fas fa-angle-down" />
-              <select
-                value={this.props.value}
-                onChange={this.props.onChange}
-                name={this.props.name}
-              >
-                {this.props.periods.map((period) => {
-                  return (
-                    <option key={period.period} value={period.period}>
-                      {period.text}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-          )}
+          <Link to="/">
+            <div>The Eye Witness Report</div>
+          </Link>
+          {period_section}
         </div>
       </div>
     );
