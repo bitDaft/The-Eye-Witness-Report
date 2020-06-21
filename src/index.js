3b47d6ca4198a45805f518d504dcc370df2ffd35
Tausif ali <ajaxhis@tutanota.com>
2020-06-21 12:16:41 +0530

    added store for redux to app

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "components/app/App";
 import * as serviceWorker from "./serviceWorker";
-
 import { BrowserRouter as Router } from "react-router-dom";
 
+import thunkMiddleware from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+import newsApp from "store/reducer";
+
+const store = createStore(newsApp, {}, applyMiddleware(thunkMiddleware));
+
+
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
